0055484ba8c603eb1594bdd91da6cbddabe59ca5
RkeyProg <rkey_prog@mail.ru>
2025-02-25 13:25:36 +0300

    feat: add AI generated title and description (need to buy tokens)

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -35,6 +35,7 @@ import {
 	CopyIcon,
 	Globe2Icon,
 	ImagePlusIcon,
+	Loader2Icon,
 	LockIcon,
 	MoreVerticalIcon,
 	RotateCcwIcon,
@@ -97,6 +98,39 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
 		},
 		onError: () => {
 			toast.error("Failed to remove video");
+		},
+	});
+
+	const generateDescription = trpc.videos.generateDescription.useMutation({
+		onSuccess: () => {
+			toast.success("Background job started", {
+				description: "This may take some time",
+			});
+		},
+		onError: () => {
+			toast.error("Failed to restore thumbnail");
+		},
+	});
+
+	const generateTitle = trpc.videos.generateTitle.useMutation({
+		onSuccess: () => {
+			toast.success("Background job started", {
+				description: "This may take some time",
+			});
+		},
+		onError: () => {
+			toast.error("Failed to restore thumbnail");
+		},
+	});
+
+	const generateThumbnail = trpc.videos.generateThumbnail.useMutation({
+		onSuccess: () => {
+			toast.success("Background job started", {
+				description: "This may take some time",
+			});
+		},
+		onError: () => {
+			toast.error("Failed to restore thumbnail");
 		},
 	});
 
@@ -180,8 +214,25 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
 								render={({ field }) => (
 									<FormItem>
 										<FormLabel>
-											Title
-											{/* TODO: add AI generate button */}
+											<div className='flex items-center gap-x-2'>
+												Title
+												<Button
+													size='icon'
+													variant='outline'
+													type='button'
+													className='rounded-full size-6 [&_svg]:size-3'
+													onClick={() => generateTitle.mutate({ id: video.id })}
+													disabled={
+														generateTitle.isPending || !video.muxTrackId
+													}
+												>
+													{generateTitle.isPending ? (
+														<Loader2Icon className='animate-spin' />
+													) : (
+														<SparklesIcon />
+													)}
+												</Button>
+											</div>
 										</FormLabel>
 										<FormControl>
 											<Input
@@ -199,8 +250,27 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
 								render={({ field }) => (
 									<FormItem>
 										<FormLabel>
-											Description
-											{/* TODO: add AI generate button */}
+											<div className='flex items-center gap-x-2'>
+												Description
+												<Button
+													size='icon'
+													variant='outline'
+													type='button'
+													className='rounded-full size-6 [&_svg]:size-3'
+													onClick={() =>
+														generateDescription.mutate({ id: video.id })
+													}
+													disabled={
+														generateDescription.isPending || !video.muxTrackId
+													}
+												>
+													{generateDescription.isPending ? (
+														<Loader2Icon className='animate-spin' />
+													) : (
+														<SparklesIcon />
+													)}
+												</Button>
+											</div>
 										</FormLabel>
 										<FormControl>
 											<Textarea
@@ -246,7 +316,11 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
 															<ImagePlusIcon className='size-4 mr-1' />
 															Change
 														</DropdownMenuItem>
-														<DropdownMenuItem>
+														<DropdownMenuItem
+															onClick={() =>
+																generateThumbnail.mutate({ id: videoId })
+															}
+														>
 															<SparklesIcon className='size-4 mr-1' />
 															AI-generated
 														</DropdownMenuItem>
